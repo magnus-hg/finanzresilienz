c1bfe1f3c0c9b3bf0b9c217b162e8a25689c3acb
magnus-hg <accounts@heitzler-geoinformatik.de>
2025-11-25 13:30:55 +0100

    fixed bugs in models.py

--- capital_market/models.py
+++ capital_market/models.py
@@ -3,19 +3,19 @@ class CapitalMarketInvestment:
         self.name = name
         self.isin = isin
         self.expected_return = expected_return
-		self.current_value = 0
-		self.current_year = -1
+        self.current_value = 0
+        self.current_year = -1
     
     
-	def simulate_year(self, investment_amount):
-		self.current_value += investment_amount
-        self.current_value += self.current_value * (1 + expected_return)
-		self.current_year += 1
+    def simulate_year(self, investment_amount):
+        self.current_value += investment_amount
+        self.current_value += self.current_value * (1 + self.expected_return)
+        self.current_year += 1
         
         return self.current_value, self.current_year
         
         
-def simulate_market_investment(name, isin, expected_return, , initial_investment_amount, yearly_investment_rate, years):
+def simulate_market_investment(name, isin, expected_return, initial_investment_amount, yearly_investment_rate, years):
     cmi = CapitalMarketInvestment(name, isin, expected_return)
     values = []
     for year in range(years):
