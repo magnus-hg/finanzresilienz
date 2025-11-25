c256bc816779bc99af4da2f38dc498a17989aee0
magnus-hg <accounts@heitzler-geoinformatik.de>
2025-11-25 14:48:57 +0100

    added preliminary real estate market information

---
+++ real_estate/market_data.py
@@ -0,0 +1,14 @@
+
+
+
+def get_real_estate_objects(region):
+    if region == "germany":
+        pass
+        
+        
+    else:
+        
+        pass
+        
+    
+    
--- real_estate/models.py
+++ real_estate/models.py
@@ -176,3 +176,48 @@ class SelfUsedPropertyInvestment:
         opportunity_cost = equity_contribution * ((1 + self.opportunity_cost_rate) ** target_years - 1)
 
         return transaction_costs + maintenance + interest_paid + opportunity_cost - imputed_savings
+
+
+
+
+
+
+
+
+
+
+
+
+
+class RealEstateInvestment:
+    def __init__(self, name, isin, expected_return):
+        self.name = name
+        self.isin = isin
+        self.expected_return = expected_return
+        self.current_value = 0
+        self.current_year = -1
+    
+    
+    def simulate_year(self, investment_amount):
+        self.current_value += investment_amount
+        self.current_value += self.current_value * self.expected_return
+        self.current_year += 1
+        
+        return self.current_value, self.current_year
+        
+        
+def simulate_market_investment(name, isin, expected_return, initial_investment_amount, yearly_investment_rate, years):
+    cmi = CapitalMarketInvestment(name, isin, expected_return)
+    values = []
+    for year in range(years):
+        if year == 0:
+            current_value, _ = cmi.simulate_year(initial_investment_amount + yearly_investment_rate)
+            values.append(current_value)
+        else:
+            current_value, _ = cmi.simulate_year(yearly_investment_rate)
+            values.append(current_value)
+
+    return values, years
+
+
+
